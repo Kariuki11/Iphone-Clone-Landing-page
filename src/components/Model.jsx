741c700091bc250382c2e46f328b39bdcd05e726
Kariuki 11 <irungukenneth433@gmail.com>
2024-12-02 15:40:02 +0300

    Add ModelView component and integrate it into Model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,22 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-
+import ModelView from "./ModelView"
+import { useState } from "react"
+import { yellowImg } from "../utils"
 
 const Model = () => {
+    const [size, setSize] = useState('small');
+    const [model, setModel] = useState({
+        title: 'Iphone 16 pro in Neutral Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg
+    })
+
+    //Camera Control
+
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
     useGSAP(() => {
         gsap.to('#heading', { y: 0, opacity: 1 })
     }, [])
@@ -13,7 +27,7 @@ const Model = () => {
 
             <div className="flex flex-col items-center mt-5">
                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                    
+                    <ModelView/>
                 </div>
             </div>
         </div>
